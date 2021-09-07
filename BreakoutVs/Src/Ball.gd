8fbe77d52d305d76107f898237b6293fdbaaa2c9
Sherman Rose <sherman@shermanrose.uk>
2021-09-07 11:58:27 +0100

    Added changing ball speed.

--- BreakoutVs/Src/Ball.gd
+++ BreakoutVs/Src/Ball.gd
@@ -3,11 +3,15 @@ extends KinematicBody2D
 class_name Ball
 
 enum BallOwner { Neutral, Red, Blue }
+
 var lastTouch = BallOwner.Neutral 
+export var ballSpeedup = .001
+var bricksBroke = 0
 
 onready var sprite = get_node("./Sprite")
 
 var velocity = Vector2(200,200)
+export var maxSpeed = 500
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("balls")
@@ -32,6 +36,11 @@ func _physics_process(delta):
 		if(collisionInfo.collider.is_in_group("bricks")):
 			if(lastTouch != BallOwner.Neutral):
 				collisionInfo.collider.destroy_brick(lastTouch)
+				print(velocity.length())
+				if(velocity.length() < maxSpeed):
+					bricksBroke += 1
+					velocity += velocity * (bricksBroke * ballSpeedup)
+					
 		
 		if(collisionInfo.collider.name == "bottomWall"):
 			GameSignals.emit_signal("lifeLost", collisionInfo.collider.name)
